1ec076461734d5234705bc7cd3d57bcf09063795
DanielCL <danielcabrera1998yt@gmail.com>
2025-09-23 03:16:40 +0000

    mmmm, si me parece bien

--- src/app/order/[id]/page.tsx
+++ src/app/order/[id]/page.tsx
@@ -15,7 +15,7 @@ export default function OrderPage({ params }: OrderPageProps) {
   // en Client Components como este, las props ya vienen resueltas.
   // Pasamos el ID directamente al componente OrderView.
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <AppHeader />
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
             <OrderView orderIdOrTableId={params.id} />
